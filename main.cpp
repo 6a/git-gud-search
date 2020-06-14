12d43209e6c85b3576ea53f85b1a2f606ecb04a1
James Stanton <jestanton@outlook.com>
2020-06-14 23:05:39 +0100

    working on binary search test

--- main.cpp
+++ main.cpp
@@ -7,10 +7,27 @@
 int testBinarySearch()
 {
     int result = -1;
-    std::vector<std::vector<int>> cases
+    size_t caseNumber = 0;
+
+    std::vector<bs::binarySearchTest> cases
     {
-        
+        bs::binarySearchTest{std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4, 3},
     };
+
+    for (auto iter = cases.begin(); iter != cases.end(); ++iter)
+    {
+        bool pass = bs::binarySearch((*iter).collection, (*iter).targetValue) == (*iter).expectedIndex;
+
+        if (!pass)
+        {
+            result = caseNumber;
+            
+            break;
+        }
+
+        ++caseNumber;
+    }
+    
 
     return result;
 }
