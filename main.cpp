5571f81f1199a2d8f66cf3958096b692990bea3e
James Stanton <jestanton@outlook.com>
2020-06-14 22:25:13 +0100

    added binary search impl. first attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include "./utility/io.h"
+
+#include "./algorithms/binarysearch.h"
+
+// Runs binary search test functions. 
+// Returns -1 if all tests pass, else returns the index of the failed test.
+int testBinarySearch()
+{
+    int result = -1;
+    std::vector<std::vector<int>> cases
+    {
+        
+    };
+
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    int binarySearchTestResult = testBinarySearch();
+    utility::outputResult("Binary Search", binarySearchTestResult);
 
+    return 0;
 }
