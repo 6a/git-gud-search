f71b42e7530774b241509dab2d127f7488c5d403
James Stanton <jestanton@outlook.com>
2020-06-14 23:05:23 +0100

    added binarysearch to bs namespace, added test case container

--- algorithms/binarysearch.h
+++ algorithms/binarysearch.h
@@ -2,44 +2,53 @@
 
 #include <vector>
 
-// Search through a sorted vector and return the index of the target, or -1 if not found.
-// Will not provide accurate results for an unsorted array.
-int binarySearch(std::vector<int>& inVector, int target)
+namespace bs
 {
-    // Start with the default value, returned when the target is not found.
-    int out = -1;
-
-    // Early exit with the default value if the input vector is empty.
-    if (inVector.size() == 0) return out;
-
-    // Early exit by returning the only value in the input vector if its of size 1.
-    if (inVector.size() == 1) return inVector[0];
-
-    // Initial values.
-    std::vector<int>::iterator low = inVector.begin();
-    std::vector<int>::iterator mid = inVector.begin();
-    std::vector<int>::iterator high = inVector.end();
-
-    // be
-    while (low <= high)
+    struct binarySearchTest
     {
-        // Determine the middle element of the vector.
-        mid = low + (high - low) / 2;
+        std::vector<int> collection;
+        int targetValue;
+        int expectedIndex;
+    };
 
-        if (target == *mid)
+    // Search through a sorted vector and return the index of the target, or -1 if not found.
+    // Will not provide accurate results for an unsorted array.
+    int binarySearch(const std::vector<int> &inVector, int target)
+    {
+        // Start with the default value, returned when the target is not found.
+        int out = -1;
+
+        // Early exit with the default value if the input vector is empty.
+        if (inVector.size() == 0) return out;
+
+        // Early exit by returning the only value in the input vector if its of size 1.
+        if (inVector.size() == 1) return inVector[0];
+
+        // Initial values.
+        std::vector<int>::const_iterator low = inVector.begin();
+        std::vector<int>::const_iterator mid = inVector.begin();
+        std::vector<int>::const_iterator high = inVector.end();
+
+        while (low <= high)
         {
-            out = *mid;
-            break;
+            // Determine the middle element of the vector.
+            mid = low + (high - low) / 2;
+
+            if (target == *mid)
+            {
+                out = *mid;
+                break;
+            }
+            else if (target < *mid)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
         }
-        else if (target < *mid)
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
-        }
+
+        return out;
     }
-
-    return out;
 }
