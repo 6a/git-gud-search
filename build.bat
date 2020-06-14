96e1a2545678baaa043c15e05d25fb5ac8b1cf08
James Stanton <jestanton@outlook.com>
2020-06-14 22:51:23 +0100

    Added running to build file

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 :: set the path to your visual studio vcvars script, it is different for every version of Visual Studio.
 set VS2019TOOLS="C:\Program Files (x86)\Visual Studio Community 2019\VC\Auxiliary\Build\vcvars64.bat"
 
+:: set the name of the output executable
+set EXECUTABLE="%~dp0/main.exe"
+
 :: make sure we found them
 if not exist %VS2019TOOLS% (
     echo VS 2019 Build Tools are missing!
@@ -12,5 +15,12 @@ call %VS2019TOOLS%
 
 :: run the compiler with your arguments
 cl.exe /EHsc /Zi /Fe: main.exe main.cpp
+
+:: if the exe exists, run it
+if exist %EXECUTABLE% (
+    echo.
+    echo.
+    %EXECUTABLE%
+)
 
 exit
