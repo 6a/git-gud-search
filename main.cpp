04c5a0e5cccf3d225b84f07433b3c23ffa0c77c1
James Stanton <jestanton@outlook.com>
2020-06-16 19:39:18 +0100

    test name changes

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 
 #include "./algorithms/binarysearch.h"
 #include "./algorithms/linearsearch.h"
+#include "./algorithms/jumpsearch.h"
 
 int main()
 {
-    utility::outputResult("Binary Search", bs::testBinary(1));
+    utility::outputResult("Binary Search", bs::test(1));
 
-    utility::outputResult("Binary Search (Recursive)", bs::testBinaryRecursive(2));
+    utility::outputResult("Binary Search (Recursive)", bs::testRecursive(2));
 
-    utility::outputResult("Linear Search", ls::testLinear(3));
+    utility::outputResult("Linear Search", ls::test(3));
+    
+    utility::outputResult("Jump Search", js::test(4));
 
     return 0;
 }
